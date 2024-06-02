8f1ee96d9b3306b1c1be05d4afb0649e9e8f9cf6
tudan110 <wtudan@126.com>
2024-06-02 21:22:55 +0800

    init

---
+++ packages/vue-api-ui/KeyValueTable.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <el-table
+        :data="keyValueData"
+        height="286"
+        border
+        style="width: 100%">
+      <el-table-column
+          prop="key"
+          label="Key"
+      >
+        <vue-editable-cell :show-input="true" slot-scope="{row}" v-model="row.key"
+                           placeholder="Key">
+          <span slot="content">{{ row.key }}</span>
+        </vue-editable-cell>
+      </el-table-column>
+      <el-table-column
+          prop="value"
+          label="Value"
+      >
+        <vue-editable-cell :show-input="true" slot-scope="{row}" v-model="row.value"
+                           placeholder="Value">
+          <span slot="content">{{ row.value }}</span>
+        </vue-editable-cell>
+      </el-table-column>
+      <el-table-column
+          v-if="!readOnly"
+          header-align="center"
+          align="center"
+          label="操作"
+          width="66">
+        <template slot="header">
+          <el-tooltip class="item" effect="light" content="添加" placement="bottom">
+            <el-button
+                type="primary"
+                icon="el-icon-plus"
+                size="mini"
+                round
+                plain
+                @click="addRow()"
+            >
+            </el-button>
+          </el-tooltip>
+        </template>
+        <template slot-scope="{row, $index}">
+          <el-tooltip class="item" effect="light" content="删除" placement="bottom">
+            <el-button
+                size="small"
+                type="text"
+                style="color: #ed4014"
+                @click="deleteRow(row, $index)"
+            >
+              <i class="el-icon-delete" title="删除"/>
+            </el-button>
+          </el-tooltip>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'KeyValueTable',
+  props: {
+    value: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+    readOnly: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  computed: {
+    keyValueData: {
+      get() {
+        return this.value
+      },
+      set(value) {
+        this.$emit('update:value', value)
+      }
+    }
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    addRow() {
+      this.keyValueData.push({
+        key: null,
+        value: null,
+        editMode: true
+      })
+    },
+    deleteRow(row, index) {
+      this.keyValueData.splice(index, 1)
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- packages/vue-api-ui/VueApiUi.vue
+++ packages/vue-api-ui/VueApiUi.vue
@@ -1,105 +1,73 @@
 <template>
   <div>
-    <div>VueApiUi</div>
 
     <!-- URL -->
     <div class="request-url">
-      <el-input placeholder="请输入内容" v-model="httpUrl" class="input-with-select">
-        <el-select v-model="httpMethod" slot="prepend" placeholder="请选择">
+      <el-input placeholder="请输入内容" v-model="form.httpUrl" class="input-with-select">
+        <el-select v-model="form.httpMethod" slot="prepend" placeholder="请选择">
           <el-option label="GET" value="get"></el-option>
           <el-option label="POST" value="post"></el-option>
           <el-option label="PUT" value="put"></el-option>
           <el-option label="DELETE" value="delete"></el-option>
         </el-select>
-        <el-button slot="append">发送</el-button>
+        <el-button slot="append" @click="send()">发送</el-button>
       </el-input>
     </div>
 
     <!-- 请求头，请求体 -->
     <div class="request-content">
-      <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tabs v-model="form.activeName" @tab-click="handleClick">
 
         <el-tab-pane label="Params" name="params">
-          <el-table
-              :data="ParamsData"
-              height="250"
-              border
-              style="width: 100%">
-            <el-table-column
-                prop="key"
-                label="Key"
-            >
-            </el-table-column>
-            <el-table-column
-                prop="value"
-                label="Value"
-            >
-            </el-table-column>
-          </el-table>
+          <key-value-table :value="form.ParamsData"/>
         </el-tab-pane>
 
         <el-tab-pane label="Headers" name="headers">
-          <el-table
-              :data="HeadersData"
-              height="250"
-              border
-              style="width: 100%">
-            <el-table-column
-                prop="key"
-                label="Key"
-            >
-            </el-table-column>
-            <el-table-column
-                prop="value"
-                label="Value"
-            >
-            </el-table-column>
-          </el-table>
+          <key-value-table :value="form.HeadersData"/>
         </el-tab-pane>
 
         <el-tab-pane label="Body" name="body">
 
-          <el-radio-group v-model="radio">
-            <el-radio :label="1">none</el-radio>
-            <el-radio :label="2">form-data</el-radio>
-            <el-radio :label="3">x-www-form-urlencoded</el-radio>
-            <el-radio :label="4">json</el-radio>
-          </el-radio-group>
+          <div class="content-type">
 
-          <template v-if="radio === 2 || radio === 3">
-            <el-table
-                :data="FormData"
-                height="250"
-                border
-                style="width: 100%">
-              <el-table-column
-                  prop="key"
-                  label="Key"
-              >
-              </el-table-column>
-              <el-table-column
-                  prop="value"
-                  label="Value"
-              >
-              </el-table-column>
-            </el-table>
+            <el-radio-group v-model="form.contentType">
+              <el-radio :label="1">none</el-radio>
+              <el-radio :label="2">form-data</el-radio>
+              <el-radio :label="3">x-www-form-urlencoded</el-radio>
+              <el-radio :label="4">json</el-radio>
+            </el-radio-group>
+
+            <el-button
+                v-if="form.contentType === 4"
+                type="primary"
+                round
+                @click="beautify()"
+            >格式化
+            </el-button>
+
+          </div>
+
+          <template v-if="form.contentType === 2 || form.contentType === 3">
+            <key-value-table :value="form.FormData"/>
           </template>
 
-          <template v-if="radio === 4">
+          <template v-if="form.contentType === 4">
             <el-input
                 type="textarea"
                 :rows="6"
                 placeholder="RequestBody"
-                v-model="body">
+                v-model="form.body"
+                style="margin-top: 10px">
             </el-input>
           </template>
 
         </el-tab-pane>
+
       </el-tabs>
     </div>
 
     <!-- 响应结果 -->
-    <div class="response-content">
+    <div class="response-content" v-if="showResponse">
       <el-input
           type="textarea"
           :rows="6"
@@ -115,47 +83,86 @@
 import Vue from 'vue'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import VueEditableCell from '@tudan110/vue-editable-cell'
+import KeyValueTable from './KeyValueTable'
 
 // Element.install(Vue)
 Vue.use(Element, {size: 'small', zIndex: 3000})
+Vue.use(VueEditableCell, {})
 
 export default {
   name: 'VueApiUi',
+  components: {
+    KeyValueTable
+  },
+  props: {
+    readOnly: {
+      type: Boolean,
+      default: false,
+    },
+    showResponse: {
+      type: Boolean,
+      default: true,
+    },
+    value: {
+      type: Object,
+      default: function () {
+        return {
+          httpUrl: null,
+          httpMethod: null,
+          activeName: 'params',
+          ParamsData: [{
+            key: 'name',
+            value: '王小虎',
+          }, {
+            key: 'age',
+            value: '20',
+          }],
+          HeadersData: [{
+            key: 'Content-Type',
+            value: 'application/json',
+          }, {
+            key: 'Connection',
+            value: 'keep-alive',
+          }],
+          FormData: [{
+            key: 'test1',
+            value: 'application',
+          }, {
+            key: 'test2',
+            value: 'asdfasdf',
+          }],
+          contentType: 1,
+          body: '{"deviceIp":"127.0.0.1","deviceId":"hj3452jgh345bghj345khj2345","serialNo":"1"}'
+        }
+      }
+    }
+  },
+  computed: {
+    form: {
+      get() {
+        return this.value
+      },
+      set(value) {
+        this.$emit('update:value', value)
+      }
+    }
+  },
   data() {
     return {
-      httpUrl: null,
-      httpMethod: null,
-      activeName: 'params',
-      ParamsData: [{
-        key: 'name',
-        value: '王小虎',
-      }, {
-        key: 'age',
-        value: 20,
-      }],
-      HeadersData: [{
-        key: 'Content-Type',
-        value: 'application/json',
-      }, {
-        key: 'Connection',
-        value: 'keep-alive',
-      }],
-      FormData: [{
-        key: 'test1',
-        value: 'application',
-      }, {
-        key: 'test2',
-        value: 'asdfasdf',
-      }],
-      radio: 1,
-      body: null,
       response: null
     }
   },
   methods: {
     handleClick(tab, event) {
       console.log(tab, event)
-    }
+    },
+    send() {
+      console.log('form', this.form)
+    },
+    beautify() {
+      this.form.body = JSON.stringify(JSON.parse(this.form.body), null, 4)
+    },
   }
 }
 </script>
@@ -173,6 +180,11 @@ export default {
   margin-top: 16px;
 }
 
+.content-type {
+  display: flex;
+  justify-content: space-between;
+}
+
 .request-content {
   margin-top: 16px;
 }
@@ -180,5 +192,35 @@ export default {
 .response-content {
   margin-top: 16px;
 }
+
+/* 全局滚动条样式 start */
+::v-deep ::-webkit-scrollbar {
+  /*滚动条整体样式*/
+  /*高宽分别对应横竖滚动条的尺寸*/
+  width: 10px;
+  height: 10px;
+}
+
+::v-deep ::-webkit-scrollbar-thumb {
+  /*滚动条里面小方块*/
+  border-radius: 10px;
+  box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+  background: #c1c1c1;
+}
+
+::v-deep ::-webkit-scrollbar-thumb:hover {
+  /*鼠标放上去滚动条颜色*/
+  background: #7d7d7d
+}
+
+::v-deep ::-webkit-scrollbar-track {
+  /*滚动条里面轨道*/
+  box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+  background: #ededed;
+  margin: 4px 0;
+}
+
+/* 全局滚动条样式 end */
 
 </style>
